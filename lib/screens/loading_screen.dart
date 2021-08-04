f7f9d733e5378d01603836b432ed925777f56b88
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2021-08-04 15:05:22 +0200

    Added an API-call

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -18,6 +19,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
+  }
+
+  void getDate() async {
+    http.Response response = await http.get(
+        'api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key}');
   }
 
   @override
