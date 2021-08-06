d3e99d0accd27e1bfd78d897e878012d6a8b7f6b
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2021-08-06 14:38:00 +0200

    Added an if

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -23,11 +23,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getDate() async {
     http.Response response = await http.get(
-        'api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key}');
+        'api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=fc89c96352d0003cb9ffa7d52460a70f');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getDate();
     return Scaffold();
   }
 }
