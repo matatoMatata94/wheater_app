c682d85bc0e13cabe545775a1a75dbacd1a293eb
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2021-08-16 23:24:26 +0200

    Getting API to work

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -22,11 +24,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getDate() async {
-    http.Response response = await http.get(
-        'api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=fc89c96352d0003cb9ffa7d52460a70f');
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
 
     if (response.statusCode == 200) {
       String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+      print(cityName);
+      print(temperature);
+      print(condition);
     } else {
       print(response.statusCode);
     }
