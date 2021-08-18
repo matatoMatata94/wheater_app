a9206873062c6ff411b34ea7b7aba3e5597cb023
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2021-08-18 23:23:34 +0200

    Did the bare minimum

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -4,12 +4,17 @@ import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+const apiKey = 'fc89c96352d0003cb9ffa7d52460a70f';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
@@ -19,13 +24,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+    getDate();
   }
 
   void getDate() async {
     http.Response response = await http.get(Uri.parse(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+        'https://samples.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
 
     if (response.statusCode == 200) {
       String data = response.body;
@@ -45,7 +51,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getDate();
     return Scaffold();
   }
 }
